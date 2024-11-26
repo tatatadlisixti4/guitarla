18735f0c3ab969208e29cc3e3d71d8692a18357c
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-25 21:16:57 -0300

    Renombrando class a className del proyecto y creando header.jsx con el contenido header el app.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,21 @@
 function Header() {
     return (
-        <>
-            <header className="py-5 header">
-                <div className="container-xl">
-                    <div className="row justify-content-center justify-content-md-between">
-                        <div className="col-8 col-md-3">
-                            <a href="index.html">
-                                <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo"/>
-                            </a>
-                        </div>
-                        <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                            <div
-                                className="carrito"
-                            >
-                                <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito"/>
+        <header className="py-5 header">
+            <div className="container-xl">
+                <div className="row justify-content-center justify-content-md-between">
+                    <div className="col-8 col-md-3">
+                        <a href="index.html">
+                            <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo"/>
+                        </a>
+                    </div>
+                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                        <div className="carrito">
+                            <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito"/>
 
-                                <div id="carrito" className="bg-white p-3">
-                                    <p className="text-center">El carrito esta vacio</p>
-                                    <table className="w-100 table">
-                                        <thead>
+                            <div id="carrito" className="bg-white p-3">
+                                <p className="text-center">El carrito esta vacio</p>
+                                <table className="w-100 table">
+                                    <thead>
                                         <tr>
                                             <th>Imagen</th>
                                             <th>Nombre</th>
@@ -26,16 +23,14 @@ function Header() {
                                             <th>Cantidad</th>
                                             <th></th>
                                         </tr>
-                                        </thead>
-                                        <tbody>
+                                    </thead>
+                                    <tbody>
                                         <tr>
                                             <td>
                                                 <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra"/>
                                             </td>
                                             <td>SRV</td>
-                                            <td className="fw-bold">
-                                                $299
-                                            </td>
+                                            <td className="fw-bold">$299</td>
                                             <td className="flex align-items-start gap-4">
                                                 <button
                                                     type="button"
@@ -60,18 +55,16 @@ function Header() {
                                                 </button>
                                             </td>
                                         </tr>
-                                        </tbody>
-                                    </table>
-                                    <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
-                                    <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
-                                </div>
+                                    </tbody>
+                                </table>
+                                <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                                <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                             </div>
-                        </nav>
-                    </div>
+                        </div>
+                    </nav>
                 </div>
-            </header>
-
-        </>
+            </div>
+        </header>
     )
 }
 
